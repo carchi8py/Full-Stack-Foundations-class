6b60c419afd5c59c6625c4fcb1eff306009b6909
Chris Archibald <carchi8py@gmail.com>
2016-03-14 19:11:27 -0700

    Starting our puppies queries

---
+++ puppy/mypup.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from puppies import Base, Shelter, Puppy
+
+import datetime
+
+engine = create_engine('sqlite:///puppyshelter.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+print 'hi'
+
+puppies = session.query(Puppy).order_by(Puppy.name).all()
+for puppy in puppies:
+	print puppy.name
+
+sixMonthOld = (datetime.date.today() - datetime.timedelta(6*365/12)).isoformat()
+
+puppies = session.query(Puppy).filter(Puppy.dateOfBirth > sixMonthOld)
+for puppy in puppies:
+	print puppy.name
+	print puppy.dateOfBirth
+	print "\n"
+
+puppies = session.query(Puppy).order_by(Puppy.weight).all()
+for puppy in puppies:
+	print puppy.name
+	print puppy.weight
+	print "\n"

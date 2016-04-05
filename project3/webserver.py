6602553e0e0653260347741accaede4dbc4368cc
Chris Archibald <carchi8py@gmail.com>
2016-04-04 17:29:39 -0700

    Added Edit and Delete buttons, they currently don't do anything

--- project3/webserver.py
+++ project3/webserver.py
@@ -51,6 +51,10 @@ class webserverHandler(BaseHTTPRequestHandler):
 				for restaurant in restaurants:
 					output += restaurant.name
 					output += "</br>"
+					output += "<a href='#'>Edit</a>"
+					output += "</br>"
+					output += "<a href='#'>Delete</a>"
+					output += "</br></br>"
 				output += "</body></html>"
 				self.wfile.write(output)
 
